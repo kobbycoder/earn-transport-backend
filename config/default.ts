fb423c87377673003c46d064154f18545246f4af
alexanderosman11 <alexanderosman11@gmail.com>
2023-05-04 12:50:55 +0000

    Database creds leak fix

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 export default {
   port: 8080,
-  dbUri: 'mongodb+srv://earntransport:ajLeosL5Ihziz90b@cluster0.eoapey5.mongodb.net/?retryWrites=true&w=majority'
+  dbUri: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eoapey5.mongodb.net/?retryWrites=true&w=majority`
 }
--- dist/config/default.js
+++ dist/config/default.js
@@ -1,6 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 exports.default = {
     port: 8080,
-    dbUri: 'mongodb+srv://earntransport:ajLeosL5Ihziz90b@cluster0.eoapey5.mongodb.net/?retryWrites=true&w=majority'
+    dbUri: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.eoapey5.mongodb.net/?retryWrites=true&w=majority`
 };
